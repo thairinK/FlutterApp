0147ee86c05b387958fbac7c856c9e80aa2baa17
Thairin Kongdee <thairink12@gmail.com>
2025-07-12 11:11:35 +0700

    reverting the commit

--- android/app/src/main/kotlin/com/example/nw/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.nw
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- api/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',      // ใส่ของคุณ
-  database: 'demo',
-});
-module.exports = pool.promise();
